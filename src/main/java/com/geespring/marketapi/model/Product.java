cb7f3f44abe117e30e29dd14e72438803058451f
GrizzlyEntertainment <47308540+GrizzlyEntertainment@users.noreply.github.com>
2022-05-27 18:58:14 -0500

    Removed redundant table name annotation and fixed wildcards

--- src/main/java/com/geespring/marketapi/model/Product.java
+++ src/main/java/com/geespring/marketapi/model/Product.java
@@ -3,12 +3,15 @@ package com.geespring.marketapi.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 @Getter
 @Setter
 @Entity
-@Table(name = "product")
 public class Product {
 
     /**
